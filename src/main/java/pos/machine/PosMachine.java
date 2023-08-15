a088c38bdd10d7b8e0e7c18d31f16d851f5e870a
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-15 18:10:59 +0800

    feat: added function to filter the unique barcodes

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,16 @@
 package pos.machine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
+    }
+
+    private List<String> filterUniqueBarcodes(List<String> barcodes){
+        return barcodes.stream().distinct().collect(Collectors.toList());
     }
 }
