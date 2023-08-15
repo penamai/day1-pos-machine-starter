7fd505ecbb70ff17253866f87a557fc16e4986ce
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-15 19:11:41 +0800

    feat: added function to generate receipt

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -19,6 +19,21 @@ public class PosMachine {
         return quantity;
     }
 
+    private String generateReceipt(List<String> uniqueBarcodes, HashMap<String, Integer> quantity, List<Item> items) {
+        StringBuilder output = new StringBuilder("***<store earning no money>Receipt***\n");
+        for (int index = 0; index < uniqueBarcodes.size(); index++) {
+            String barcode = uniqueBarcodes.get(index);
+            String itemName = getItemName(barcode, items);
+            int itemQuantity = quantity.get(barcode);
+            int itemPrice = getItemPrice(barcode, items);
+            int itemSubtotal = getSubtotal(itemQuantity, itemPrice);
+
+            output.append(generateOneItem(itemName, itemQuantity, itemPrice, itemSubtotal));
+        }
+        output.append("----------------------\nTotal: 24 (yuan)\n**********************");
+        return output.toString();
+    }
+
     private String getItemName(String barcode, List<Item>items){
         return Objects.requireNonNull(items.stream().filter(item -> barcode.equals(item.getBarcode())).findAny().orElse(null)).getName();
     }
